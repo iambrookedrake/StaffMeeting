02cd2c4843f7e0f425183d39b8fcc589a624a12f
iambrookedrake <61724789+iambrookedrake@users.noreply.github.com>
2020-06-22 20:10:40 -0600

    Update run.py

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ from pages import index, predictions, insights, process
 
 # Navbar docs: https://dash-bootstrap-components.opensource.faculty.ai/l/components/navbar
 navbar = dbc.NavbarSimple(
-    brand='Check In?',
+    brand='Work Smarter',
     brand_href='/', 
     children=[
         dbc.NavItem(dcc.Link('Predictions', href='/predictions', className='nav-link')), 
